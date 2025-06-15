1c38e414388d6fb06d043305e0cedabc4ee15493
Katrincik <grebennicova.ecaterina@gmail.com>
2025-06-15 21:04:40 +0300

    Changed the faq section for ipad width

--- scripts/faq.js
+++ scripts/faq.js
@@ -1,33 +1,32 @@
 const ARRAY_OF_ELEMENTS = [
 	{
 		title: "Alpine Bus Journeys?",
-		content: `Forget crowded cities! Alpine Bus Journeys are your comfy seat
-								with panoramic windows to breathtaking mountain vistas, charming
-								villages, and air so fresh it might make you yodel (please
-								don't, unless you're outside). It's transportation with a side
-								of unparalleled scenery (and maybe a hairpin turn).`,
+		content: `Forget crowded cities! Alpine Bus Journeys are your comfy seat with panoramic windows to breathtaking mountain vistas, charming villages, and air so fresh it might make you yodel (please don't, unless you're outside). It's transportation with a side of unparalleled scenery (and maybe a hairpin turn).`,
 	},
 	{
 		title: "What about seating options?",
-		content: `What about seating options?
+		content: `
 Generally, you'll find comfy standard seats (great views, great value, maybe great new friends) and perhaps slightly better seats with extra legroom. All offer a front-row seat to the greatest show on Earth – the Alps!`,
 	},
 	{
 		title: "What is the Alpine Wallet?",
-		content: `What is the Alpine Wallet?
+		content: `
 It's your digital stash of cash that makes booking bus tickets smoother than a perfectly paved mountain road. Load it up and watch your travel dreams become reality faster than a bus goes downhill.`,
 	},
 	{
 		title: "Why a Bus Station Fee?",
-		content: `What is the Alpine Wallet?
-My Altitude Score?
+		content: `
 Your loyalty points for cruising the mountain roads! Higher scores might get you priority boarding (less elbowing for the best window seat!) or maybe just bragging rights among fellow peak-passengers who prefer four wheels`,
 	},
 	{
 		title: "Vitae sollicitudin vitae libero tincidunt",
 		content: `
-Why a Bus Station Fee?
 A small contribution to ensure the station is clean, has reasonably fresh mountain air (filtered from bus fumes), and enough signs pointing you towards your bus, not away from it. Sometimes covers the cost of moving rogue bicycles.`,
+	},
+	{
+		title: "Why are buses late?",
+		content: `
+Sometimes it's the weather (snow happens!), sometimes a sudden flock of particularly slow-moving sheep on the road, and sometimes the driver just had to stop for a photo of that view. Or maybe they got stuck behind a tractor. Welcome to "Mountain Road Time"!`,
 	},
 ];
 
